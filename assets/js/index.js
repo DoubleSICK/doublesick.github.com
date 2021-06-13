61e6dc11780c5dd056ef119bee5324e154aaa2b1
DoubleSICK <doublesick@2261.org.cn>
2021-06-13 23:09:07 +0800

    add page

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+function initSwiper() {
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true, // 循环模式选项
+
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // 如果需要滚动条
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+}
+
+function init() {
+  initSwiper();
+}
+
+window.onload = function () {
+  init();
+};
