f00af4297f91f002a74a3bb62358acb3699aea0d
echowtyang <echowtyang@tencent.com>
2021-06-18 01:13:43 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
+function initVideo() {
+  var player = new Txplayer({
+    containerId: 'player',
+    vid: 'd3253lcqpgj',
+    width: '100%',
+    height: '100%',
+    autoplay: true,
+  });
+}
+
 function initSwiper() {
   var mySwiper = new Swiper('.swiper-container', {
     loop: true, // 循环模式选项
@@ -21,6 +31,7 @@ function initSwiper() {
 }
 
 function init() {
+  initVideo();
   initSwiper();
 }
 
